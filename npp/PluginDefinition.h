ad9ceaf5e783b484038f1d9ec8918fe6c74937dc
Simon Steinberger <steinberger.simon@gmail.com>
2026-09-21 11:49:43 +0200

    Rename all occurrences to NPPHighlighter without dash

--- npp/PluginDefinition.h
+++ npp/PluginDefinition.h
@@ -28,7 +28,7 @@
 //-------------------------------------//
 // Here define your plugin name
 //
-const TCHAR NPP_PLUGIN_NAME[] = TEXT("Npp-Highlighter");
+const TCHAR NPP_PLUGIN_NAME[] = TEXT("NppHighlighter");
 
 //-----------------------------------------------//
 //-- STEP 2. DEFINE YOUR PLUGIN COMMAND NUMBER --//
